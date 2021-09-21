7fe2fcc770f0b0009d34a3e6094d49e4458e7782
yinping-520 <85038945+yinping-520@users.noreply.github.com>
2021-09-21 09:38:26 -0400

    port changed

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const fs = require('fs')
 const path = require("path")
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 const data = require("./db/db.json")
 
